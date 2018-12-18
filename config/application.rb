16ca084165550cc71c25a42a6a94999f5669d88e
EmCousin <emmanuel_cousin@hotmail.fr>
2018-12-18 10:17:35 -0200

    allow iframe origin from dani-and-manu.info

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module Covoisite
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.action_dispatch.default_headers.merge!({
+      'X-Frame-Options' => 'ALLOW-FROM http://dani-and-manu.info/'
+    })
   end
 end
