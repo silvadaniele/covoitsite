e01a08086e4cea263b9fd20f3e3552a042b3b54f
EmCousin <emmanuel_cousin@hotmail.fr>
2019-02-24 11:51:05 +0100

    edit mail setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
     address: ENV['MAILGUN_SMTP_SERVER'],
     user_name: ENV['MAILGUN_SMTP_LOGIN'],
     password: ENV['MAILGUN_SMTP_PASSWORD'],
-    domain: ENV['RAILS_HOST'],
+    domain: ENV['MAILGUN_DOMAIN'],
     authentication: :plain,
   }
   config.action_mailer.delivery_method = :smtp
